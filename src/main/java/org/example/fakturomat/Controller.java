b622e8f15740d0171e2152a91a04d2b3e36ee93a
FuarkBruh <k.dorszewski.00@gmail.com>
2024-04-10 16:16:41 +0200

    Poprawa komentarza dla testu

--- src/main/java/org/example/fakturomat/Controller.java
+++ src/main/java/org/example/fakturomat/Controller.java
@@ -3,7 +3,7 @@ package org.example.fakturomat;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class Controller {
     @FXML
     private Label welcomeText;
 
