208d569fccb0685a871b009f7771671932177e33
FuarkBruh <k.dorszewski.00@gmail.com>
2024-04-17 16:02:44 +0200

    Wprowadzanie dodawania dynamicznego kolejnych pozycji do faktury oraz ustawienie identyfikatorów losowanych przez UUID (Universally Unique Identifier)

--- src/main/java/org/example/fakturomat/Controller.java
+++ src/main/java/org/example/fakturomat/Controller.java
@@ -4,6 +4,10 @@ import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+import java.util.UUID;
 
 public class Controller {
     @FXML
@@ -32,6 +36,38 @@ public class Controller {
     private TextField cenaNetto;
     @FXML
     private ComboBox<String> stawkaVAT;
+    @FXML
+    private VBox poziomyVBox;
+    @FXML
+    private HBox poziomyHBox;
+    @FXML
+    protected void dodajPozycje() {
+        // Tworzenie nowych kontrolki i nadawanie im unikatowych fx:id
+        TextField nazwaTowaru = new TextField();
+        nazwaTowaru.setId("nazwaTowaru_" + UUID.randomUUID().toString());
+
+        ComboBox<String> jednostkaMiary = new ComboBox<>();
+        jednostkaMiary.setId("jednostkaMiary_" + UUID.randomUUID().toString());
+
+        TextField ilosc = new TextField();
+        ilosc.setId("ilosc_" + UUID.randomUUID().toString());
+
+        TextField cenaNetto = new TextField();
+        cenaNetto.setId("cenaNetto_" + UUID.randomUUID().toString());
+
+        ComboBox<String> stawkaVAT = new ComboBox<>();
+        stawkaVAT.setId("stawkaVAT_" + UUID.randomUUID().toString());
+
+        // Tworzenie nowego HBox dla kolejnej pozycji towarowej
+        HBox nowyPoziomyHBox = new HBox();
+        nowyPoziomyHBox.getChildren().addAll(nazwaTowaru, jednostkaMiary, ilosc, cenaNetto, stawkaVAT);
+
+        // Dodanie HBox do VBox
+        poziomyVBox.getChildren().add(nowyPoziomyHBox);
+    }
+
+
+
 
     @FXML
     protected void onGenerujButtonClick() {
