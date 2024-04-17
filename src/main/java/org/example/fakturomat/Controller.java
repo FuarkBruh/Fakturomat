641cea6ede26954150a43ce3acc3cf536525ca82
FuarkBruh <k.dorszewski.00@gmail.com>
2024-04-17 17:01:19 +0200

    Zapis danych faktury poprzez użycie List

--- src/main/java/org/example/fakturomat/Controller.java
+++ src/main/java/org/example/fakturomat/Controller.java
@@ -7,6 +7,8 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 public class Controller {
@@ -40,34 +42,46 @@ public class Controller {
     private VBox poziomyVBox;
     @FXML
     private HBox poziomyHBox;
+    private List<TextField> listaNazwaTowaru = new ArrayList<>();
+    private List<ComboBox<String>> listaJednostkaMiary = new ArrayList<>();
+    private List<TextField> listaIlosc = new ArrayList<>();
+    private List<TextField> listaCenaNetto = new ArrayList<>();
+    private List<ComboBox<String>> listaStawkaVAT = new ArrayList<>();
     @FXML
     protected void dodajPozycje() {
+        String idLokalne = String.valueOf(UUID.randomUUID());
+
         // Tworzenie nowych kontrolki i nadawanie im unikatowych fx:id
         TextField nowaNazwaTowaru = new TextField();
         nowaNazwaTowaru.setPrefHeight(nazwaTowaru.getPrefHeight());
         nowaNazwaTowaru.setPrefWidth(nazwaTowaru.getPrefWidth());
-        nowaNazwaTowaru.setId("nazwaTowaru_" + UUID.randomUUID());
+        nowaNazwaTowaru.setId("nazwaTowaru_" + idLokalne);
+        listaNazwaTowaru.add(nowaNazwaTowaru);
 
         ComboBox<String> nowaJednostkaMiary = new ComboBox<>();
         nowaJednostkaMiary.setPrefHeight(jednostkaMiary.getPrefHeight());
         nowaJednostkaMiary.setPrefWidth(jednostkaMiary.getPrefWidth());
         nowaJednostkaMiary.setItems(jednostkaMiary.getItems());
-        nowaJednostkaMiary.setId("jednostkaMiary_" + UUID.randomUUID());
+        nowaJednostkaMiary.setId("jednostkaMiary_" + idLokalne);
+        listaJednostkaMiary.add(nowaJednostkaMiary);
 
         TextField nowaIlosc = new TextField();
         nowaIlosc.setPrefHeight(ilosc.getPrefHeight());
         nowaIlosc.setPrefWidth(ilosc.getPrefWidth());
-        nowaIlosc.setId("ilosc_" + UUID.randomUUID());
+        nowaIlosc.setId("ilosc_" + idLokalne);
+        listaIlosc.add(nowaIlosc);
 
         TextField nowaCenaNetto = new TextField();
         nowaCenaNetto.setPrefHeight(cenaNetto.getPrefHeight());
         nowaCenaNetto.setPrefWidth(cenaNetto.getPrefWidth());
-        nowaCenaNetto.setId("cenaNetto_" + UUID.randomUUID());
+        nowaCenaNetto.setId("cenaNetto_" + idLokalne);
+        listaCenaNetto.add(nowaCenaNetto);
 
         ComboBox<String> nowaStawkaVAT = new ComboBox<>();
         nowaStawkaVAT.setPrefHeight(stawkaVAT.getPrefHeight());
         nowaStawkaVAT.setPrefWidth(stawkaVAT.getPrefWidth());
-        nowaStawkaVAT.setId("stawkaVAT_" + UUID.randomUUID());
+        nowaStawkaVAT.setId("stawkaVAT_" + idLokalne);
+        listaStawkaVAT.add(nowaStawkaVAT);
 
         // Tworzenie nowego HBox dla kolejnej pozycji towarowej
         HBox nowyPoziomyHBox = new HBox();
@@ -80,7 +94,8 @@ public class Controller {
 
     @FXML
     protected void onGenerujButtonClick() {
-        PDFGenerator.generatePDF(numerFaktury, dataWystawienia, dataSprzedazy, nabywca, nip, ulica, miasto, osobaWystawiajaca,
-                nazwaTowaru, jednostkaMiary, ilosc, cenaNetto, stawkaVAT);
+        PDFGenerator.generatePDF(numerFaktury, dataWystawienia, dataSprzedazy, nabywca, nip, ulica, miasto,
+                osobaWystawiajaca, nazwaTowaru, jednostkaMiary, ilosc, cenaNetto, stawkaVAT, listaNazwaTowaru,
+                listaIlosc, listaJednostkaMiary, listaCenaNetto, listaStawkaVAT);
     }
 }
--- src/main/java/org/example/fakturomat/PDFGenerator.java
+++ src/main/java/org/example/fakturomat/PDFGenerator.java
@@ -11,52 +11,69 @@ import org.apache.pdfbox.pdmodel.font.PDType0Font;
 import java.io.File;
 import java.io.IOException;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 public class PDFGenerator {
     public static void generatePDF(TextField numerFaktury, DatePicker dataWystawienia, DatePicker dataSprzedazy,
                                    TextField nabywca, TextField nip, TextField ulica, TextField miasto,
                                    ComboBox<String> osobaWystawiajaca,
                                    TextField nazwaTowaru, ComboBox<String> jednostkaMiary,
-                                   TextField ilosc, TextField cenaNetto, ComboBox<String> stawkaVAT) {
-        try (PDDocument document = new PDDocument()) {
+                                   TextField ilosc, TextField cenaNetto, ComboBox<String> stawkaVAT,
+                                   List<TextField> listaNazwaTowaru, List<TextField> listaIlosc,
+                                   List<ComboBox<String>> listaJednostkaMiary, List<TextField> listaCenaNetto,
+                                   List<ComboBox<String>> listaStawkaVAT) {
+        try {
+            PDDocument document = new PDDocument();
             PDPage page = new PDPage();
             document.addPage(page);
 
-            try (PDPageContentStream contentStream = new PDPageContentStream(document, page)) {
-                // Bardzo ważne, aby plik .ttf był, bo inaczej program nie obsługuje np. kropki i wywala błędy
-                contentStream.setFont(PDType0Font.load(document, new File("TimesNewRoman.ttf")), 12);
-                contentStream.beginText();
-                contentStream.newLineAtOffset(50, 700);
-                contentStream.showText("Numer faktury: " + numerFaktury.getText());
-                contentStream.newLineAtOffset(0, -20);
-                contentStream.showText("Data wystawienia: " + dataWystawienia.getValue().format(DateTimeFormatter.ofPattern("dd-MM-yyyy")));
-                contentStream.newLineAtOffset(0, -20);
-                contentStream.showText("Data sprzedaży: " + dataSprzedazy.getValue().format(DateTimeFormatter.ofPattern("dd-MM-yyyy")));
-                contentStream.newLineAtOffset(0, -20);
-                contentStream.showText("Nabywca: " + nabywca.getText());
-                contentStream.newLineAtOffset(0, -20);
-                contentStream.showText("NIP: " + nip.getText());
-                contentStream.newLineAtOffset(0, -20);
-                contentStream.showText("Ulica: " + ulica.getText());
-                contentStream.newLineAtOffset(0, -20);
-                contentStream.showText("Miasto: " + miasto.getText());
-                contentStream.newLineAtOffset(0, -20);
-                contentStream.showText("Osoba wystawiająca: " + osobaWystawiajaca.getValue());
-                contentStream.newLineAtOffset(0, -20);
-                contentStream.showText("Nazwa towaru: " + nazwaTowaru.getText());
-                contentStream.newLineAtOffset(0, -20);
-                contentStream.showText("Jednostka miary: " + jednostkaMiary.getValue());
-                contentStream.newLineAtOffset(0, -20);
-                contentStream.showText("Ilość: " + ilosc.getText());
-                contentStream.newLineAtOffset(0, -20);
-                contentStream.showText("Cena netto: " + cenaNetto.getText());
-                contentStream.newLineAtOffset(0, -20);
-                contentStream.showText("Stawka VAT: " + stawkaVAT.getValue());
+            PDPageContentStream contentStream = new PDPageContentStream(document, page);
+            contentStream.setFont(PDType0Font.load(document, new File("TimesNewRoman.ttf")), 12);
+            contentStream.beginText();
+            contentStream.newLineAtOffset(50, 700);
+            contentStream.showText("Numer faktury: " + numerFaktury.getText());
+            contentStream.newLineAtOffset(0, -20);
+            contentStream.showText("Data wystawienia: " + dataWystawienia.getValue().format(DateTimeFormatter.ofPattern("dd-MM-yyyy")));
+            contentStream.newLineAtOffset(0, -20);
+            contentStream.showText("Data sprzedaży: " + dataSprzedazy.getValue().format(DateTimeFormatter.ofPattern("dd-MM-yyyy")));
+            contentStream.newLineAtOffset(0, -20);
+            contentStream.showText("Nabywca: " + nabywca.getText());
+            contentStream.newLineAtOffset(0, -20);
+            contentStream.showText("NIP: " + nip.getText());
+            contentStream.newLineAtOffset(0, -20);
+            contentStream.showText("Ulica: " + ulica.getText());
+            contentStream.newLineAtOffset(0, -20);
+            contentStream.showText("Miasto: " + miasto.getText());
+            contentStream.newLineAtOffset(0, -20);
+            contentStream.showText("Osoba wystawiająca: " + osobaWystawiajaca.getValue());
+            contentStream.newLineAtOffset(0, -20);
+            contentStream.showText("Nazwa towaru: " + nazwaTowaru.getText());
+            contentStream.newLineAtOffset(0, -20);
+            contentStream.showText("Jednostka miary: " + jednostkaMiary.getValue());
+            contentStream.newLineAtOffset(0, -20);
+            contentStream.showText("Ilość: " + ilosc.getText());
+            contentStream.newLineAtOffset(0, -20);
+            contentStream.showText("Cena netto: " + cenaNetto.getText());
+            contentStream.newLineAtOffset(0, -20);
+            contentStream.showText("Stawka VAT: " + stawkaVAT.getValue());
 
-                contentStream.endText();
+            for (int i = 0; i < listaNazwaTowaru.size(); i++) {
+                contentStream.newLineAtOffset(0, -20);
+                contentStream.showText("Nazwa towaru: " + listaNazwaTowaru.get(i).getText());
+                contentStream.newLineAtOffset(0, -20);
+                contentStream.showText("Jednostka miary: " + listaJednostkaMiary.get(i).getValue());
+                contentStream.newLineAtOffset(0, -20);
+                contentStream.showText("Ilość: " + listaIlosc.get(i).getText());
+                contentStream.newLineAtOffset(0, -20);
+                contentStream.showText("Cena netto: " + listaCenaNetto.get(i).getText());
+                contentStream.newLineAtOffset(0, -20);
+                contentStream.showText("Stawka VAT: " + listaStawkaVAT.get(i).getValue());
             }
+            contentStream.endText();
+            contentStream.close();
 
             document.save("faktura.pdf");
+            document.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
